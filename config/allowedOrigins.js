8f3dba545753e3d4d2d498cceec5933b246c3f62
s.ponsupee@gmail.com <s.ponsupee@gmail.com>
2024-06-16 19:41:57 +0700

    deploy

---
+++ config/allowedOrigins.js
@@ -0,0 +1,14 @@
+const allowedOrigins = [
+    'https://tongna.vercel.app',
+    'https://backendtop5insure.devphin.tech' ,
+    'http://127.0.0.1:5500' ,
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:9500',
+    'http://localhost:3002',
+    'http://localhost:3005',
+    'http://localhost:6001',
+    'http://146.190.103.25'
+];
+
+module.exports = allowedOrigins;
